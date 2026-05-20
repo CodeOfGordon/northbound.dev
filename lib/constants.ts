576f63f191f24f1e965143a1a78772bb78929b2b
Code_Of_Gordon <gordonhuynh2@gmail.com>
2026-05-20 19:02:21 -0400

    First

---
+++ lib/constants.ts
@@ -0,0 +1,62 @@
+export const events = [
+  {
+    image: '/images/event1.png',
+    title: 'Toronto JavaScript Meetup',
+    slug: 'toronto-javascript-meetup',
+    company: 'Toronto.js Community',
+    country: 'Canada',
+    city: 'Toronto',
+    date: 'June 15, 2026',
+    time: '6:00 PM'
+  },
+  {
+    image: '/images/event2.png',
+    title: 'PyData Toronto Conference',
+    slug: 'pydata-toronto-2026',
+    company: 'PyData',
+    country: 'Canada',
+    city: 'Toronto',
+    date: 'July 8, 2026',
+    time: '9:00 AM'
+  },
+  {
+    image: '/images/event3.png',
+    title: 'React Toronto Developers Meetup',
+    slug: 'react-toronto-meetup',
+    company: 'React Toronto',
+    country: 'Canada',
+    city: 'Toronto',
+    date: 'June 22, 2026',
+    time: '6:30 PM'
+  },
+  {
+    image: '/images/event4.png',
+    title: 'Toronto DevOps & Cloud Meetup',
+    slug: 'toronto-devops-meetup',
+    company: 'DevOps Toronto',
+    country: 'Canada',
+    city: 'Toronto',
+    date: 'July 1, 2026',
+    time: '5:30 PM'
+  },
+  {
+    image: '/images/event5.png',
+    title: 'GTA Game Developers Conference',
+    slug: 'gta-gamedev-conf',
+    company: 'GTA Gamedev Community',
+    country: 'Canada',
+    city: 'Mississauga',
+    date: 'August 10, 2026',
+    time: '10:00 AM'
+  },
+  {
+    image: '/images/event6.png',
+    title: 'Toronto Web Development Hackathon',
+    slug: 'toronto-web-hackathon',
+    company: 'Tech Toronto',
+    country: 'Canada',
+    city: 'Toronto',
+    date: 'July 25-26, 2026',
+    time: '9:00 AM'
+  }
+];
